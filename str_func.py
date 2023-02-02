8e646c1728b07127f4f081624e073ecd12fc1b61
Olga_Pos <olga-posp@yandex.ru>
2023-02-02 22:44:42 +0100

    add second function in str_func.py and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def capital_letters(a) -> str:
+    """
+    Description
+    """
     return a.upper()
+
+
+def every_first_letter_caps(b) -> str:
+    """
+    Function modifies the string - every word starts with capital letter
+    """
+    return b.title()
